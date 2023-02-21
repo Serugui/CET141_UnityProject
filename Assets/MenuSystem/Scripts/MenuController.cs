f5d2d20c5455bfff5b9f5ed2c8f9b6f0c3f95a2f
Sergi Ramon <bi23tl@student.sunderland.ac.uk>
2023-02-21 10:16:50 +0000

    Menu

---
+++ Assets/MenuSystem/Scripts/MenuController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] string playScene = "PlayerSandboxScene";
+    [SerializeField] string mainMenuScene = "StartScene";
+}
