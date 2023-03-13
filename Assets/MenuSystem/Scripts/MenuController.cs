82945b4a74a2f316b362e7bbeadd343fc35fbb0d
Sergi Ramon <115111327+Serugui@users.noreply.github.com>
2023-03-13 12:20:23 +0000

    Delete Assets directory

--- Assets/MenuSystem/Scripts/MenuController.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MenuController : MonoBehaviour
-{
-    [SerializeField] string playScene = "PlayerSandboxScene";
-    [SerializeField] string mainMenuScene = "StartScene";
-}
